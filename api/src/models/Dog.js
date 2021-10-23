819a15af22c4d31939030fa88d13512ea7b50505
Derek_Cabrera <cabrera.derek.adriel@gmail.com>
2021-10-23 20:38:43 -0300

    models realizados

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,46 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      primaryKey:true
+    },
+    //nombre
     name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    //altura
+    height: { 
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    //peso 
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    //años devida
+    yearsOfLife: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    img: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   });
-};
+  sequelize.define('temperament', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey:true
+      
+    },
+    nombre:{
+      type: DataTypes.STRING,
+      allowNull:false
+    }
+  });
+}
